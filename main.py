333410d8c77cfdd402cece97662bab59f5833c58
felix1007 <>
2022-09-14 17:30:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("wellcome")
+name = input("enter your name ")
+print("hi " + name)
+code = input(name + " pls enter your new code ")
+enterde_code =  input(name + " enter pass code ")
+if  code == enterde_code:
+  print("good now.... ")
+  
+else:
+  print("wrong")
